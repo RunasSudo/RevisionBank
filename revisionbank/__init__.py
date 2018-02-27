8eb6b295a2a225efd7927f35dafaff918a8e8b92
RunasSudo <runassudo@yingtongli.me>
2018-02-27 19:33:49 +1100

    Initial commit

---
+++ revisionbank/__init__.py
@@ -0,0 +1,38 @@
+#   RevisionBank
+#   Copyright © 2018  Yingtong Li (RunasSudo)
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import flask
+
+from flask_pymongo import PyMongo
+
+import revisionbank.page
+
+app = flask.Flask(__name__)
+mongo = PyMongo(app)
+
+@app.route('/')
+def index():
+	return flask.render_template('index.html')
+
+@app.route('/page/<path:page_name>')
+def page_view(page_name):
+	page_json = mongo.db.pages.find_one({'name': page_name})
+	if page_json is None:
+		page = revisionbank.page.Page404(name=page_name)
+	else:
+		page = revisionbank.page.Page(**page_json)
+	
+	return flask.render_template('page.html', page=page)
---
+++ revisionbank/page.py
@@ -0,0 +1,33 @@
+#   RevisionBank
+#   Copyright © 2018  Yingtong Li (RunasSudo)
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import flask
+import jinja2
+
+class BasePage:
+	def __init__(self, name=None):
+		self.name = name
+	
+	@property
+	def pretty_name(self):
+		return self.name.split('/')[-1]
+
+class Page404(BasePage):
+	def render_content(self):
+		return jinja2.Markup(flask.render_template('page_404.html'))
+
+class PageMarkdown(BasePage):
+	pass
