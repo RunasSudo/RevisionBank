36c846ebf068c594c0fec8777b89a18e7b42ea4a
RunasSudo <runassudo@yingtongli.me>
2018-03-10 16:44:30 +1100

    Implement authentication and basic editing

--- revisionbank/__init__.py
+++ revisionbank/__init__.py
@@ -16,11 +16,21 @@
 
 import flask
 
+from flask_oauthlib.client import OAuth
 from flask_pymongo import PyMongo
 
 import revisionbank.page
+import revisionbank.user
+
+from datetime import datetime
+import os
+import pytz
 
 app = flask.Flask(__name__)
+
+if 'REVBANK_SETTINGS' in os.environ:
+	app.config.from_envvar('REVBANK_SETTINGS')
+
 mongo = PyMongo(app)
 
 @app.route('/')
@@ -33,16 +43,91 @@ def page_view(page_name):
 	if page_json is None:
 		page = revisionbank.page.Page404(name=page_name)
 	else:
-		page = revisionbank.page.PageMarkdown(**page_json)
+		page = revisionbank.page.Page.from_json(page_json)
 	
 	return flask.render_template('page.html', page=page)
 
-@app.route('/page/<path:page_name>/edit')
+@app.route('/page/<path:page_name>/edit', methods=['GET', 'POST'])
 def page_edit(page_name):
-	page_json = mongo.db.pages.find_one({'name': page_name})
-	if page_json is None:
-		page = revisionbank.page.PageMarkdown(name=page_name)
+	if flask.request.method == 'GET':
+		page_json = mongo.db.pages.find_one({'name': page_name})
+		if page_json is None:
+			page = revisionbank.page.Page(name=page_name)
+		else:
+			page = revisionbank.page.Page.from_json(page_json)
+		
+		return flask.render_template('page_edit.html', page=page)
 	else:
-		page = revisionbank.page.PageMarkdown(**page_json)
+		page_json = mongo.db.pages.find_one({'name': page_name})
+		if page_json is None:
+			page = revisionbank.page.Page(name=page_name)
+		else:
+			page = revisionbank.page.Page.from_json(page_json)
+		
+		revision = revisionbank.page.RevisionMarkdown(creator=current_user(), creation_date=pytz.utc.localize(datetime.utcnow()), content=flask.request.form['page-content'], reason=flask.request.form['edit-reason'])
+		
+		page.revisions.append(revision)
+		
+		mongo.db.pages.replace_one({'name': page_name}, page.to_json(), upsert=True)
+		
+		return flask.redirect(flask.url_for('page_view', page_name=page_name))
+
+oauth = OAuth(app)
+google = oauth.remote_app(
+	'google',
+	consumer_key=app.config.get('GOOGLE_ID'),
+	consumer_secret=app.config.get('GOOGLE_SECRET'),
+	request_token_params={
+		'scope': 'email profile',
+		'hd': app.config.get('GOOGLE_HD')
+	},
+	base_url='https://www.googleapis.com/oauth2/v1/',
+	request_token_url=None,
+	access_token_method='POST',
+	access_token_url='https://accounts.google.com/o/oauth2/token',
+	authorize_url='https://accounts.google.com/o/oauth2/auth',
+)
+
+@app.route('/auth/tmp')
+def auth_tmp():
+	if 'google_token' in flask.session:
+		me = google.get('userinfo')
+		return flask.jsonify({"data": me.data})
+	return flask.redirect(flask.url_for('login'))
+
+@app.route('/auth/login')
+def login():
+	return google.authorize(callback=flask.url_for('authorized', _external=True))
+
+@app.route('/auth/logout')
+def logout():
+	flask.session.pop('google_token', None)
+	flask.session.pop('google_user', None)
+	return flask.redirect(flask.url_for('index'))
+
+@app.route('/auth/authorized')
+def authorized():
+	resp = google.authorized_response()
+	if resp is None:
+		return 'Access denied'
+	flask.session['google_token'] = (resp['access_token'], '')
+	me = google.get('userinfo')
 	
-	return flask.render_template('page_edit.html', page=page)
+	if me.data['hd'] == app.config.get('GOOGLE_HD') or me.data['hd'].endswith('.' + app.config.get('GOOGLE_HD')):
+		pass
+	else:
+		# Invalid domain
+		flask.session.pop('google_token', None)
+		return 'Access denied: Invalid domain'
+	
+	flask.session['google_user'] = me.data
+	return flask.redirect(flask.url_for('index'))
+
+@google.tokengetter
+def get_google_oauth_token():
+	return flask.session.get('google_token')
+
+def current_user():
+	if 'google_token' not in flask.session:
+		return None
+	return revisionbank.user.GoogleUser(name=flask.session['google_user']['name'], email=flask.session['google_user']['email'])
--- revisionbank/page.py
+++ revisionbank/page.py
@@ -14,20 +14,92 @@
 #   You should have received a copy of the GNU Affero General Public License
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+from datetime import datetime
 import flask
 import jinja2
+import pytz
+import mwparserfromhell as mw
 
-class BasePage:
+class MongoObject:
+	@classmethod
+	def from_json(cls, json_obj):
+		return cls(**json_obj)
+
+class BasePage(MongoObject):
 	def __init__(self, name=None):
 		self.name = name
 	
 	@property
 	def pretty_name(self):
 		return self.name.split('/')[-1]
+	
+	def to_json(self):
+		return {'name': self.name}
 
 class Page404(BasePage):
 	def render_content(self):
 		return jinja2.Markup(flask.render_template('page_404.html', page=self))
 
-class PageMarkdown(BasePage):
-	pass
+class Page(BasePage):
+	def __init__(self, _id=None, revisions=None, **kwargs):
+		super().__init__(**kwargs)
+		if revisions is None:
+			revisions = []
+		self.revisions = revisions
+	
+	def render_content(self):
+		return self.revisions[-1].render_content()
+	
+	def to_json(self):
+		json_obj = super().to_json()
+		json_obj.update({'revisions': [r.to_json() for r in self.revisions]})
+		return json_obj
+	
+	@classmethod
+	def from_json(cls, json_obj):
+		obj = Page(**json_obj)
+		obj.revisions = [RevisionMarkdown.from_json(r) for r in obj.revisions]
+		return obj
+
+class Revision(MongoObject):
+	def __init__(self, creator=None, creation_date=None, comment=None, reason=None):
+		self.creator = creator
+		self.creation_date = creation_date
+		self.comment = comment
+		self.reason = reason
+	
+	def to_json(self):
+		return {'creator': self.creator.to_json(), 'creation_date': self.creation_date.strftime('%Y-%m-%dT%H:%M:%SZ'), 'comment': self.comment, 'reason': self.reason}
+	
+	@classmethod
+	def from_json(cls, json_obj):
+		import revisionbank.user
+		
+		obj = cls(**json_obj)
+		obj.creator = revisionbank.user.GoogleUser.from_json(obj.creator)
+		obj.creation_date = pytz.utc.localize(datetime.strptime(json_obj['creation_date'], '%Y-%m-%dT%H:%M:%SZ'))
+		return obj
+
+class RevisionMarkdown(Revision):
+	def __init__(self, content=None, **kwargs):
+		super().__init__(**kwargs)
+		self.content = content
+	
+	def render_node(self, node):
+		if isinstance(node, mw.nodes.text.Text):
+			markup = jinja2.escape(node.value)
+		elif isinstance(node, mw.wikicode.Wikicode):
+			markup = jinja2.Markup()
+			for subnode in node.ifilter():
+				markup += self.render_node(subnode)
+		return markup
+	
+	def render_content(self):
+		wikitext = mw.parse(self.content)
+		markup = self.render_node(wikitext)
+		return markup
+	
+	def to_json(self):
+		json_obj = super().to_json()
+		json_obj.update({'content': self.content})
+		return json_obj
